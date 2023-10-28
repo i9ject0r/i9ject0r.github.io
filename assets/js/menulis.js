3a9ca7506d67c2e658ac8504140b85be4e9e2a12
i9ject0r <97001931+i9ject0r@users.noreply.github.com>
2023-10-28 16:30:17 +0800

    Add files via upload

---
+++ assets/js/menulis.js
@@ -0,0 +1,6 @@
+new TypeIt("#tulis", {
+    strings: "#hack2protect",
+    speed: 350,
+    loop : true,
+    waitUntilVisible: true,
+  }).go();
---
+++ assets/js/title.js
@@ -0,0 +1,6 @@
+new TypeIt("#title", {
+    strings: "0xi9ject0r",
+    speed: 350,
+    loop : true,
+    waitUntilVisible: true,
+  }).go();
